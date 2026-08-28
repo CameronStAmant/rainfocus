7399bde1b2ed7282265e095cb371b68e50c6026f
Cameron St Amant <40364114+CameronStAmant@users.noreply.github.com>
2026-08-28 03:08:57 -0600

    Configure app to build into a single file

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
 import path from "node:path";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    viteSingleFile(),
+  ],
   base: "/rainfocus/",
   resolve: {
     alias: {
